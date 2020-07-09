636251791acb36e7f62236ec9832b8e393228a1f
yetingmao <yetingmao@163.com>
2020-07-09 14:30:58 +0800

    增加新题

--- README.md
+++ README.md
@@ -152,3 +152,9 @@
 ##### 给你一个数组 nums 。数组「动态和」的计算公式为：runningSum[i] = sum(nums[0]…nums[i]) 。请返回 nums 的动态和。
 [Code](https://github.com/yetingmao/leetcode_javascript/blob/master/easy/21_30.js)
 ************
+
+####  拥有最多糖果的孩子
+##### 给你一个数组 candies 和一个整数 extraCandies ，其中 candies[i] 代表第 i 个孩子拥有的糖果数目。
+##### 对每一个孩子，检查是否存在一种方案，将额外的 extraCandies 个糖果分配给孩子们之后，此孩子有 最多 的糖果。注意，允许有多个孩子同时拥有 最多 的糖果数目。
+[Code](https://github.com/yetingmao/leetcode_javascript/blob/master/easy/21_30.js)
+************
--- easy/21_30.js
+++ easy/21_30.js
@@ -3,10 +3,10 @@
  * @Autor: yetm
  * @Date: 2020-07-09 11:07:35
  * @LastEditors: yetm
- * @LastEditTime: 2020-07-09 11:08:23
+ * @LastEditTime: 2020-07-09 14:30:50
  */
 /**
- * @description: 给你一个数组 nums 。数组「动态和」的计算公式为：runningSum[i] = sum(nums[0]…nums[i]) 。请返回 nums 的动态和。
+ * @description:21 给你一个数组 nums 。数组「动态和」的计算公式为：runningSum[i] = sum(nums[0]…nums[i]) 。请返回 nums 的动态和。
  * @param{number[]} nums
  * @return: {number[]}
  * @author: yetm
@@ -19,4 +19,16 @@ var runningSum = function (nums) {
     });
     result.push(last);
     return result;
+};
+/**
+ * @description:22 拥有最多糖果的孩子
+ * @param {type} {number[]} candies  
+ * @param {number} extraCandies
+ * @return:  {boolean[]}
+ * @author: yetm
+ */
+var kidsWithCandies = function (candies, extraCandies) {
+    const arr = [...candies].sort((a, b) => a - b);
+    const max = arr[arr.length - 1];
+    return candies.map(item => item + extraCandies >= max);
 };
